9dd71ffb201050194c154a699457113b8f017213
Andrzej <37003497+andrew362@users.noreply.github.com>
2022-12-04 21:40:04 +0100

    error fix

--- src/components/header.js
+++ src/components/header.js
@@ -127,7 +127,7 @@ const Header = ({ history, dimensions }) => {
           },
         });
     }
-  }, [menuState]);
+  }, [menuState, dimensions.height, dimensions.width]);
 
   return (
     <div className="header">
